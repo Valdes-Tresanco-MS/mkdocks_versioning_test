f1d4284b1f8596c216f016357f374d74d0fcf6a0
Mario S. Valdés <mariosergiovaldes145@gmail.com>
2022-04-11 18:26:15 -0500

    remove an unnecessary div

--- docs/news.js
+++ docs/news.js
@@ -1,4 +1,3 @@
 data = '[{"news" : ["gmx_MMPBSA v1.5.2 is out 😀!", "News 2. Check the performance test", "News 3. Check the' +
-    ' performance test", "News 4. Check the performance test"], "urls":' +
-    ' ["https://github.com/adajhu/Protein-pKa-prediction-by-tree-based-machine-learning", "#","#","#"]},' +
-    '{"name" : "Abhinandan", "age" : "20"}]';
+    ' performance test", "News 4. Check the performance test", "News 5: Esto es una prueba"], "urls":' +
+    ' ["https://github.com/adajhu/Protein-pKa-prediction-by-tree-based-machine-learning", "#","#","#", "#"]}]';
