49b66cdbfa3c92c16167b99b5f846e31b129619b
Mario S. Valdés <mariosergiovaldes145@gmail.com>
2022-06-01 21:34:26 -0500

    improved news

--- docs/news.js
+++
@@ -1,3 +0,0 @@
-data = '[{"news" : ["gmx_MMPBSA v1.5.2 is out 😀!", "News 2. Check the performance test", "News 3. Check the' +
-    ' performance test", "News 4. Check the performance test", "News 5: Esto es una prueba"], "urls":' +
-    ' ["https://github.com/adajhu/Protein-pKa-prediction-by-tree-based-machine-learning", "#","#","#", "#"]}]';
